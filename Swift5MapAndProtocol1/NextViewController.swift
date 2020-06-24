b00f99e5f2fa3915a7d0faa12612870b5c3e9013
naokihtml <miyaki101033@gmail.com>
2020-06-25 01:04:46 +0900

    UIの作成とMapkitとCoreLocationをimportしました。

---
+++ Swift5MapAndProtocol1/NextViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NextViewController.swift
+//  Swift5MapAndProtocol1
+//
+//  Created by みや on 2020/06/25.
+//  Copyright © 2020 みや. All rights reserved.
+//
+
+import UIKit
+
+class NextViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
